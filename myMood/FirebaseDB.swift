31b76ff8d13841d753be991a8c55f9caf8d34461
Martin <m4rtin.t@gmail.com>
2017-09-05 17:57:15 -0700

    Updated insert and get entry

--- myMood/FirebaseDB.swift
+++ myMood/FirebaseDB.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import FirebaseDatabase
+import FirebaseAuth
+import MapKit
 
 final class FirebaseDBController {
     var ref:DatabaseReference!
@@ -27,25 +29,56 @@ final class FirebaseDBController {
         
         ref.child("Entries").child("Entry_1").updateChildValues(["Date":"Sample",
                                                                  "Description":"Sample",
-                                                                 "ListofPhotos":"Sample",
+                                                                 "PhotoURL":"Sample",
                                                                  "Location":"Sample",
                                                                  "Mood":"Sample"])
         
         
-        ref.child("Photos").child("Photo_1").updateChildValues(["url":"Sample"])
+        //ref.child("Photos").child("Photo_1").updateChildValues(["url":"Sample"])
+    }
+    
+    //TODO
+    //GET all data for the user
+    func getAllEntries() -> NSDictionary {
+        let userID = Auth.auth().currentUser?.uid
+        var dict:NSDictionary?
+        ref.child("Users").child(userID!).observeSingleEvent(of: .value, with: { (snapShot) in
+            //Get user value
+            dict =  (snapShot.value as! NSDictionary)
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        return dict!
     }
     
     //TODO 
     //Insert Entry
-    func insertEntry(date:Date, description:String, listOfPhotos:[Photo], mood:Int) {
-        let dateFormatter = dateFormatter()
+    func insertEntry(date:Date, description:String, photo:Photo, mood:Int, location:CLLocation) {
+        let userId = Auth.auth().currentUser?.uid
         
+        //Date
+        let dateFormatter = DateFormatter()
+        let date:String = dateFormatter.string(from: date)
         
-        ref.child("Entries").child("Entry_1").updateChildValues(["Date":"Sample",
-                                                                 "Description":"Sample",
-                                                                 "ListofPhotos":"Sample",
-                                                                 "Location":"Sample",
-                                                                 "Mood":"Sample"])
+        //TODO  Get Photo URL
+        //Add current Photos
+        
+        //Change location to string
+        let location:String = "\(location.coordinate.latitude),\(location.coordinate.longitude)"
+        
+        //Auto generate entry id
+        let newRef = ref.child("Entries").childByAutoId()
+        
+        newRef.setValue(["Date":date,
+                         "Description":description,
+                         "PhotoURL":"Sample URL",
+                         "Location":location,
+                         "Mood":mood])
+        
+        let entryID = newRef.key
+        
+        //insert entry to list of entry in user
+        ref.child("Users").child(userId!).child("ListofEntries").updateChildValues([entryID:true])
         
     }
     
