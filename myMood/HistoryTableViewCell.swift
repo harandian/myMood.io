a83f2634e709826f6cc76f3ade4029cad715f863
Martin <m4rtin.t@gmail.com>
2017-09-29 00:09:30 -0400

    clean up cells

--- myMood/HistoryTableViewCell.swift
+++ myMood/HistoryTableViewCell.swift
@@ -12,18 +12,5 @@ class HistoryTableViewCell: UITableViewCell {
     
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var moodLabel: UILabel!
-    
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-        
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
